70d9bdbd91e08afbf7e7c9c7a696e008e407aa84
neev <neevlokchandani@gmail.com>
2021-01-07 20:44:35 +0530

    imiges

--- sketch.js
+++ sketch.js
@@ -9,8 +9,18 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var i1,i2,i3,i4,i5;
 
 
+function preload(){
+  i1=loadImage("images/car1.png");
+  i2=loadImage("images/car2.png");
+  i3=loadImage("images/car3.png");
+  i4=loadImage("images/car4.png");
+  i5=loadImage("images/track.jpg");
+
+
+}
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
@@ -21,11 +31,15 @@ function setup(){
 
 
 function draw(){
+  
   if(playerCount === 4){
     game.update(1);
   }
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end();
   }
 }
